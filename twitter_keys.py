17f7d1bb4f91fef2f8f792ba2d053ad0a778c1e8
harsh1606 <harshtyagi022@gmail.com>
2020-06-30 10:21:09 +0530

    Tweet Analysis

---
+++ twitter_keys.py
@@ -0,0 +1,4 @@
+Access_token = "1275631352147111936-NsLAHPeus2aBr0iGcaJCLpS95Atneg"
+Access_token_secret = "LwuWSQ3gd9wdpPq1ksCPkRr9mFwkxiXomY0FjGkhERov1"
+Api_key = "hOVP9QUkGXbyX2oZkIQgaxga4"
+API_secret_key = "GhJ3q4uPQz5fcqyeT7iKIJD2C7cm6rLv9fBtitQpb1X3Od2KzZ"
